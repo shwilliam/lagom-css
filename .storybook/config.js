db56de6d8315e4823626adc33bee98c44e7d7bb9
William L <w-lindvall@outlook.com>
2019-04-02 00:16:36 -0700

    feat: sketch font face styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import 'loki/configure-react'
 import '../dist/lagom.min.css'
 
 function loadStories() {
+  require('../stories/fonts.js')
   require('../stories/margin.js')
   require('../stories/padding.js')
 }
