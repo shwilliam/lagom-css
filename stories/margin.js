4b774e0810f761a063b282ae9afb82c0e6d0e480
William L <w-lindvall@outlook.com>
2019-04-03 09:46:14 -0700

    test: margin left & right auto stories

--- stories/margin.js
+++ stories/margin.js
@@ -3,41 +3,62 @@ import { storiesOf } from '@storybook/react'
 
 ['', 'top', 'right', 'bottom', 'left']
   .forEach((side) => {
-    [0, 1, 2, 3, 4, 5, 6]
-      .forEach((i) => {
-        storiesOf('margin', module)
-          .add(`${
-            side
-              ? `${side} `
-              : ''
-          }${i}`, () => (
-            <span
-              style={{
-                border: '1px solid black',
-                display: 'inline-block'
-              }}
-            >
+    ['a', 0, 1, 2, 3, 4, 5, 6]
+      .forEach((modifier) => {
+        if (modifier === 'a') {
+          if (!['right', 'left'].includes(side)) {} else {
+            storiesOf('margin', module)
+              .add(`m${side[0]}${modifier}`, () => (
+                <div
+                  style={{
+                    border: '1px solid black',
+                    width: '100px'
+                  }}
+                >
+                  <div
+                    className={`m${side[0]}${modifier}`}
+                    style={{ width: 'max-content' }}
+                  >
+                    {`m${side[0]}${modifier}`}
+                  </div>
+                </div>
+              ))
+          }
+        } else {
+          storiesOf('margin', module)
+            .add(`${
+              side
+                ? `${side} `
+                : ''
+            }${modifier}`, () => (
               <span
-                className={
-                  `m${
-                    side.length > 0
-                      ? side[0]
-                      : ''
-                  }${i}`
-                }
                 style={{
+                  border: '1px solid black',
                   display: 'inline-block'
                 }}
               >
-                {
-                  `m${
-                    side.length > 0
-                      ? side[0]
-                      : ''
-                  }${i}`
-                }
+                <span
+                  className={
+                    `m${
+                      side.length > 0
+                        ? side[0]
+                        : ''
+                    }${modifier}`
+                  }
+                  style={{
+                    display: 'inline-block'
+                  }}
+                >
+                  {
+                    `m${
+                      side.length > 0
+                        ? side[0]
+                        : ''
+                    }${modifier}`
+                  }
+                </span>
               </span>
-            </span>
-          ))
+            ))
+        }
       })
   })
